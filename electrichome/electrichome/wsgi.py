2fffa1e1b4dc555171ddcdedc510c1e544e5cb13
Joesan Gabaldon <438251+joesan-g@users.noreply.github.com>
2024-02-07 12:07:33 -0500

    Added empty django project

---
+++ electrichome/electrichome/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for electrichome project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'electrichome.settings')
+
+application = get_wsgi_application()
