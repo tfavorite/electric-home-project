3b03cc4a2f8072f7245c982733d6b70fd3cffb6c
Tim Favorite <tim.favorite@gmail.com>
2024-02-08 17:27:51 -0500

    attempt to register static files

--- electrichome/electrichome/urls.py
+++ electrichome/electrichome/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#from django.conf import settings
+#from django.conf.urls.static import static
 
 from .views import my_view
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', my_view, name='my_view'),
-]
+] #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
